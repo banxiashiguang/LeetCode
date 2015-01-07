de4f11a9e72b6fbdecf51a7e60aa5d2e5472600b
banxiashiguang <731887616@qq.com>
2015-01-07 10:12:45 +0800

    LeetCode

---
+++ medianOfTwoSortArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class medianOfTwoSortArray
+{
+public:
+	medianOfTwoSortArray() { }
+	~medianOfTwoSortArray() { }
+	double solution(int A[],int m,int B[],int n); 
+};
+
+double medianOfTwoSortArray::solution(int A[],int m,int B[],int n)
+{
+	int a = 0;
+	int b = 0;
+	int count = 2;
+	int median = (m+n)/2;
+	while(a < m && b < n)
+	{
+
+	}
+}
